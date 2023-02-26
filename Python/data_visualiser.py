9c4ae0f012d8fec697a005c3fcce444b83b0e700
Andrii <anyak@ciklum.com>
2023-02-26 20:59:01 +0100

    Fixes virtualenv. Code clean-up.

--- Python/data_visualiser.py
+++ Python/data_visualiser.py
@@ -8,82 +8,59 @@ import numpy as np
 import pandas as pd
 
 from bokeh.io import output_file, show
-from bokeh.layouts import column, gridplot, row  # , widgetbox
+from bokeh.layouts import column, row  # , gridplot, widgetbox
 from bokeh.models import CheckboxGroup, ColumnDataSource, CustomJS
 from bokeh.palettes import Dark2_5 as palette, Viridis3
 from bokeh.plotting import figure
 
 DATASET_PATH = "..\\dataset\\"
 
-log_path = os.path.abspath(os.path.join(__file__, DATASET_PATH, 'log.txt'))
+log_path = os.path.abspath(os.path.join(
+    __file__, DATASET_PATH, 'log.txt'))
 out_t_path = os.path.abspath(os.path.join(
     __file__, DATASET_PATH, 'out_t-.txt'))
 loading_path = os.path.abspath(os.path.join(
     __file__, DATASET_PATH, 'loading.txt'))
 
-# def replace_tdeg(t_list):
-#     return t_list.replace('tdeg=', '')
 
-# df = pd.read_csv("log.txt")
+# Main log data
 with open(log_path, "r") as file:
     data = csv.reader(file, delimiter=" ")
 
-    # Can then load into a df if needed
+    # Can then load into a df if needed.
     df = pd.DataFrame.from_records(data)
-    # transform the first row into column indexes
 
-    # print(df)
+    # Transform the first row into column indexes.
+    new_header = df.iloc[0]  # Grab the first row for the header.
+    df = df[1:]  # Take the data less the header row.
+    df.columns = new_header  # Set the header row as the df header.
 
-    new_header = df.iloc[0]  # grab the first row for the header
-    df = df[1:]  # take the data less the header row
-    df.columns = new_header  # set the header row as the df header
-
-    # getting dataframe only for needed command (which means temperature data)
+    # Getting dataframe only for needed command (which means temperature data).
     temp_frames = df[df.COMMAND == "COMMAND:0x05"]
 
-    # getting a list of ID's with the command above
+    # Getting a list of ID's with the command above.
     devices_list = list(dict.fromkeys(temp_frames['ID'].tolist()))
-    # commands_list = list(dict.fromkeys(temp_frames['COMMAND'].tolist()))
+    # Commands_list = list(dict.fromkeys(temp_frames['COMMAND'].tolist())).
 
-    # get the dictionary with the x, y grids values tuple {"ID_key" ([x1,x2,...xn], [y1,y2,...yn])}
+    # Get the dictionary with the x, y grids values tuple {"ID_key" ([x1,x2,...xn], [y1,y2,...yn])}.
     time_series_dict = {}
     for device_id in devices_list:
         tf_device = temp_frames[temp_frames.ID == device_id]
-        # get the date and time in string
+        # Get the date and time in string.
         time_dev = list(map(lambda a, b: a+' '+b,
                         tf_device['date'].tolist(), tf_device['time'].tolist()))
-        # convert date and time to datetime format
+        # Convert date and time to datetime format.
         time_dev = list(map(lambda a: datetime.datetime.strptime(
             a, '%d.%m.%Y %H:%M:%S'), time_dev))
-        # get the temperature data
+        # Get the temperature data.
         temp_value_dev = tf_device['tdeg'].tolist()
-        # cut the crap (tdeg=) from the temperature value
+        # Cut the crap (tdeg=) from the temperature value.
         temp_value_dev = list(
             map(lambda x: x.replace('tdeg=', ''), temp_value_dev))
-        # convert string to integer
+        # Convert string to integer.
         temp_value_dev = [int(i) for i in temp_value_dev]
         time_series_dict[device_id] = (time_dev, temp_value_dev)
-# # Outer temperature data
-# with open("out_t.txt", "r") as file:
-#     data = csv.reader(file, delimiter="%")
-#     df = pd.DataFrame.from_records(data)
 
-#     first_date_m_y = ".11.2015"
-#     first_date = "02"+first_date_m_y
-
-#     out_temp_list = df.iloc[:, 1].tolist()
-
-#     day_hours = 0
-#     date_m = 2
-#     out_timedate_list = []
-#     df_time_list = df.iloc[:, 0].tolist()
-#     # add date to time list
-#     for time_str in df_time_list:
-#         # check if it's a new day
-#         if int(time_str[0:1]) < day_hours:
-#             date_m = date_m + 1
-#         day_hours = int(time_str[0:1])
-#         out_timedate_list.append(datetime.datetime.strptime(str('{:02}'.format(date_m))+first_date_m_y+" "+time_str, '%d.%m.%Y %H:%M:%S') )
 # Outer temperature data
 with open(out_t_path, "r") as file:
     data = csv.reader(file, delimiter=" ")
@@ -98,7 +75,8 @@ with open(out_t_path, "r") as file:
     out_timedate_list = list(map(lambda a: datetime.datetime.strptime(
         a, '%m/%d/%Y %H:%M:%S'), out_timedate_list))
 time_series_dict["out_temp"] = (out_timedate_list, out_temp_list)
-# Wood Loadings
+
+# Boiler Loadings
 with open(loading_path, "r") as file:
     data = csv.reader(file, delimiter=" ")
     df = pd.DataFrame.from_records(data)
@@ -113,7 +91,7 @@ with open(loading_path, "r") as file:
         a, '%m/%d/%Y %H:%M'), load_timedate_list))
 TOOLS = "pan,wheel_zoom,box_zoom,reset,save,box_select"
 
-plot1 = figure(title="Графіки нагріву котла в Каневі",
+plot1 = figure(title="Graphs from the heating system sensors.",
                x_axis_type='datetime', tools=TOOLS)
 
 line_colors = itertools.cycle(palette)
@@ -121,15 +99,15 @@ line_colors = itertools.cycle(palette)
 for dev in time_series_dict.keys():
     if dev != "ID:0xF002":
         if dev == "ID:0x3EE0":
-            label = "Door х10"
+            label = "Door, t°C х10"
         elif dev == "ID:0x2EE0":
-            label = "Room"
+            label = "Room, t°C"
         elif dev == "ID:0x4EE0":
-            label = "Feed"
+            label = "Feed, t°C"
         elif dev == "ID:0x1EE0":
-            label = "Return"
+            label = "Return, t°C"
         elif dev == "out_temp":
-            label = "Outside"
+            label = "Outside, t°C"
         else:
             label = dev
         x, y = time_series_dict[dev]
@@ -137,12 +115,8 @@ for dev in time_series_dict.keys():
             y = list(map(lambda a: int(a)/10, y))
         plot1.line(x, y, legend_label=label, color=next(line_colors))
 
-# source = ColumnDataSource(data=dict(x=load_timedate_list, counts=load_amount_list))
-# plot1.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:], fill_color="#036564", line_color="#033649")
-# plot1.vbar(width=0.9, legend_label="Loads", color=next(line_colors), source=source )
-
 plot1.circle(load_timedate_list, load_amount_list,
-             legend_label="Loads", color=next(line_colors))
+             legend_label="Loads, RefVol.", color=next(line_colors))
 
 # plot1.y_range.start = 0
 plot1.x_range.range_padding = 0.1
@@ -151,131 +125,4 @@ plot1.xaxis.major_label_orientation = 1
 
 output_file("graph.html", title="heating system log")
 
-# show(gridplot([p1, p2], ncols=2, plot_width=400, plot_height=400))  # open a browser
-
-# show(p2, plot_width=400, plot_height=400)  # open a browser
-show(plot1, plot_width=400, plot_height=400)  # open a browser
-
-'''
-# FIXME: Remember why I needed to debug this!!!
-# TODO: Check and reverse datas (make them follow from left to right)
-# TODO: Disable and enable lines
-print(df)
-time_series_dict["ID:0x5EE0"]
-# temp_frames
-devices_list
-# commands_list
-# time_series_dict
-devices_list = list(dict.fromkeys(df['ID'].tolist()))
-"""
-COMMANDS:
-0x05: temperature
-0x0B: voltage
-"""
-commands_list = list(dict.fromkeys(df['COMMAND'].tolist()))
-# t1['ID'].tolist()
-# devices_list
-commands_list
-
-gb = df.groupby('ID')
-gb.sum()
-df
-df[df.ID == "ID:0x9CC0"]
-'''
-
-# FIXME: Remove debug Kruft 
-# =========================================================================================
-# Create html files.
-output_file("line_on_off.html", title="line_on_off.py example")
-
-p = figure()
-props = dict(line_width=4, line_alpha=0.7)
-x = np.linspace(0, 4 * np.pi, 100)
-l0 = p.line(x, np.sin(x), color=Viridis3[0], legend_label="Line 0", **props)
-l1 = p.line(x, 4 * np.cos(x),
-            color=Viridis3[1], legend_label="Line 1", **props)
-l2 = p.line(x, np.tan(x), color=Viridis3[2], legend_label="Line 2", **props)
-
-checkbox = CheckboxGroup(labels=["Line 0", "Line 1", "Line 2"],
-                         active=[0, 1, 2], width=100)
-
-checkbox.js_on_change("active", CustomJS(args=dict(l0=l0, l1=l1, l2=l2, checkbox=checkbox),
-                              code="""
-l0.visible = 0 in checkbox.active;
-l1.visible = 1 in checkbox.active;
-l2.visible = 2 in checkbox.active;
-"""))
-
-layout = row(checkbox, p)
-show(layout)
-
-# =========================================================================================
-output_file("layout.html")
-
-x = list(range(11))
-y0 = x
-y1 = [10 - i for i in x]
-y2 = [abs(i - 5) for i in x]
-
-s1 = figure(min_width=250, height=250, title=None)
-s1.circle(x, y0, size=10, color="navy", alpha=0.5)
-
-s2 = figure(min_width=250, height=250, title=None)
-s2.triangle(x, y1, size=10, color="firebrick", alpha=0.5)
-
-s3 = figure(min_width=250, height=250, title=None)
-s3.square(x, y2, size=10, color="olive", alpha=0.5)
-
-col = column(s1, s2, s3)
-
-checkbox = CheckboxGroup(labels=["Plot 1", "Plot 2", "Plot 3"],
-                         active=[0, 1, 2], width=100)
-
-callback = CustomJS(args=dict(plots=[s1, s2, s3], col=col, checkbox=checkbox), code="""
-const children = []
-for (const i of checkbox.active) {
-     children.push(plots[i])
-} 
-col.children = children
-""")
-checkbox.js_on_change('active', callback)
-
-show(row(checkbox, col))
-
-# FIXME: remove debug Kruft.
-# =========================================================================================
-t = np.arange(0.0, 2.0, 0.01)
-s = np.sin(3*np.pi*t)
-c = np.cos(3*np.pi*t)
-
-source = ColumnDataSource(data=dict(t=t, s=s, c=c))
-
-plot = figure(min_width=400, height=400)
-a = plot.line('t', 's', source=source, line_width=3,
-              line_alpha=0.6, line_color='blue')
-b = plot.line('t', 'c', source=source, line_width=3,
-              line_alpha=0.6, line_color='red')
-
-checkbox2 = CheckboxGroup(labels=["Cosinus", "Sinus"], active=[0, 1])
-
-callback = CustomJS(args=dict(line0=a, line1=b), code="""
-    //console.log(cb_obj.active);
-    line0.visible = false;
-    line1.visible = false;
-    for (i in cb_obj.active) {
-        //console.log(cb_obj.active[i]);
-        if (cb_obj.active[i] == 0) {
-            line0.visible = true;
-        } else if (cb_obj.active[i] == 1) {
-            line1.visible = true;
-        }
-    }
-""")
-
-checkbox2.js_on_change('active', callback)
-
-# show(row(checkbox2, col))
-
-# TODO: implement this.
-# layout = row(plot, widgetbox(checkbox))
-# show(layout)
+show(plot1, plot_width=800, plot_height=400)  # open a browser
